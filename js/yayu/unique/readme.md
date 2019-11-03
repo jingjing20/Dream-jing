10f7d0da7d6931380d1947248446b3a71bf35683
“jingjinjingjing20 <1269928993@qq.com>
2019-11-03 17:27:16 +0800

    bonus

---
+++ js/yayu/unique/readme.md
@@ -0,0 +1,20 @@
+- js 的那些数据类型
+    数值型 Number 1
+    字符串类型 '1'
+    布尔值 true
+    undefined 未定义
+    null 空类型 赋值了，但是值为空
+    上面的都是原子类型 简单数据类型
+    Object 其他的都是对象 复杂数据类型
+        数组 可以被迭代的对象 iterable
+        函数 可以被运行的对象 function()
+        JSON 对象字面量 {}可以被key:value 的对象
+
+- 变量名name 值value
+    let func; //内存中申请分配 地址
+    undefined ？ 类型还未定义
+    js 是弱类型语言 有别于Java c++等静态类型语言
+    js 的变量类型是由值来决定的
+
+- type null 'object'?
+- 怎么样区别 [] 和 {} 的typeof?
---
+++ js/yayu/unique/unique.js
@@ -0,0 +1,40 @@
+//数组去重
+/**
+ * @param {array} arr
+ * @return {Array}
+ */
+// function unique(arr){    es5
+// function 关键字没有了 代替的是es6箭头函数 (参数) => {}
+let fn;
+console.log(typeof fn);     //类型未定义的
+fn =null;
+console.log(typeof fn);
+fn = () => {}; 
+console.log(typeof fn);
+const unique = (arr) =>{
+    let res = [];   //数组
+
+    for(var i = 0,arrLen =arr.length; i < arrLen; i++){
+        var current = arr[i];   //基地 局部变量 缓存
+        if(res.indexOf(arr[i]) == -1){          //== -1 意思是没有
+            res.push(current);
+        }
+    }
+    // for(var i = 0; i < arr.length; i++){
+    //     let isIn = false;
+    //     for(var j =0; j < res.length; j++){
+    //         if(arr[i] === res[j]){
+    //             isIn = true;
+    //             break;
+    //         }
+    //     }
+    //     if(!isIn){1
+    //     res.push(arr[i]);
+    //     }
+    // }
+    return res;
+}
+
+var arr = ['1',2,3,2,4,1];
+console.log(unique(arr));
+console.log(typeof unique == 'function' && unique(arr));
