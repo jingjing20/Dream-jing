8c53558db343fb0054345042fc1b7d638d5ac21b
e <1269928993@qq.com>
2020-01-03 22:40:51 +0800

    docker

---
+++ docker/node/dockerstudy/app.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const logger = require('koa-logger');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+// 中间件式
+// 分层
+const index = require('./routes/index')
+const fs = require('fs');
+
+onerror(app);
+// app.use(ctx => {
+//     ctx.body = fs.createReadStream('fhdsjfh');
+// })
+
+app.use(bodyparser({
+    enableTypes: ['json', 'form', 'text']
+}));
+
+app.use(require('koa-static')(__dirname + '/public'));
+app.use(views(__dirname + '/views', {
+    extension: 'pug'
+}));
+
+app.use(logger())
+app.use(index.routes(), index.allowedMethods())
+    // 模块化 commonJS
+module.exports = app;
---
+++ docker/node/dockerstudy/readme.md
@@ -0,0 +1,36 @@
+- Koa 是最流行的node 轻量级框架
+  egg.js 基于koa的二次开发
+
+- koa new -> app -> listen
+    /bin/www
+    www文件就是网站项目的启动文件
+    http.createServer(app.callback())
+
+- 模板
+    views
+    koa-views() 
+    app.use()   启用views中间件
+    模板在哪里？ 使用哪种模板引擎？ pug
+
+- 路由  分层
+    routes/index.js
+    koa-router()    .get() .post()
+    index.routes()
+    app.use()
+
+- logger
+    koa-logger  use
+
+- 静态资源 服器端资源
+    cdn机器 本地没有？去中央服务器里请求一次 缓存到本地
+
+- koa-static
+- 表单提交
+    koa-bodyparser
+    GET 请求头里 head GET
+
+## node的发布
+    docker  容器化
+    node 项目 写完后 测试工程师(没有所有的环境？)
+    让项目在不同的机器里 享用相同的环境跑起来 只需要装一个docker
+
