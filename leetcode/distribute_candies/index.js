d03dca6a0a8223b145b3651f23a80cf152e74907
e <1269928993@qq.com>
2020-01-11 22:06:43 +0800

    小修改

--- leetcode/distribute_candies/index.js
+++ leetcode/distribute_candies/index.js
@@ -6,15 +6,16 @@ var distributeCandies = (candies) => {
     let count = 0;
     //1.种类数 unique
     //出现过的？
-    let obj = {};   //对象字面量
+    let obj = {}; //对象字面量
     // candies.forEach(item => {
     //     console.log(item);
     // })
-    for(let item of candies) {
-        //console.log(item);
-        if(!obj[item]) {
+    for (let item of candies) {
+        console.log(item);
+        if (!obj[item]) {
             obj[item] = 1;
             count++;
+            // console.log(obj)
         }
     }
 
@@ -24,7 +25,7 @@ var distributeCandies = (candies) => {
     //     return count;
     // }
     //return count;
-    return (count >= candies.length>>1) ? candies.length>>1 : count;
+    return (count >= candies.length >> 1) ? candies.length >> 1 : count;
 }
 
-console.log(distributeCandies([1,1,2,2,3,3]));
+console.log(distributeCandies([1, 1, 2, 5]));
--- leetcode/distribute_candies/main.js
+++ leetcode/distribute_candies/main.js
@@ -1,9 +1,10 @@
 var distributeCandies = (candies) => {
     // 去重 数据结构 有种结构 数组容器 不重复
     const count = new Set(candies);
+    console.log(count)
     console.log(count.size);
 
-    return Math.min(count.size,candies.length>>1);  //数学对象
+    return Math.min(count.size, candies.length >> 1); //数学对象
 }
 
-distributeCandies([1,1,2,2,3,3,4,5]);
+console.log(distributeCandies([1, 1, 2, 3]))
