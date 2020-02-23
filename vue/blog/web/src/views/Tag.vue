9f717fd9bf2dea74997c5c0c1f3db40d4016ae7d
e <1269928993@qq.com>
2020-02-23 12:07:17 +0800

    vue-express

---
+++ vue/blog/web/src/views/Tag.vue
@@ -0,0 +1,19 @@
+<!--  -->
+<template>
+  <div>
+    Tag
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
