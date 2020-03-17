59ada8e1c8f19effdd793c0880f8412dd2e7b0a7
jingjing20 <1269928993@qq.com>
2020-03-17 13:09:14 +0800

    leetcode

---
+++ leetcode/3无重复字符串/1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = s => {
+  let len = s.length
+  if(len <= 1){
+    return len
+  }
+  let max = 1
+  let recode = 0
+  for(let i = 0; i < len; i++){
+    let index = s.indexOf(s[i], recode)
+    // console.log(index)
+    if(index < i) {
+      recode = index + 1
+    }
+    // console.log(recode)
+    max = Math.max(max, i - recode + 1)
+    // console.log(max)
+  }
+  return max
+};
+
+console.log(lengthOfLongestSubstring("abb"))
