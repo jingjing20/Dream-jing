8a90ba332c26511b167f790c3cc866279b6d2958
jingjing20 <1269928993@qq.com>
2020-03-17 23:02:53 +0800

    leetcode-01

---
+++ leetcode/1两数之和/3.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = new Map();
+  for(let i in nums) {
+    map.set(nums[i],i)
+  }
+  for(let j = 0; j < nums.length - 1; j++){
+    if(map.has( target - nums[j] ) && map.get( target - nums[j]) != j ){
+      return [j , parseInt(map.get( target - nums[j]))]
+    }
+  }
+}
+console.log(twoSum([2,7,11,15],22))
