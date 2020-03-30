b8c19cd094ed3ca52ee4adec777e2f01ae80af6f
jingjing20 <1269928993@qq.com>
2020-03-30 12:23:38 +0800

    css圣杯布局 this

--- study_Record/js讶羽深入系列/继承/3.js
+++ study_Record/js讶羽深入系列/继承/3.js
@@ -4,12 +4,12 @@ function Foo() {
 }
 Foo.getName = function() { console.log(2); }
 Foo.prototype.getName = function() { console.log(3); }
-var getName = function() { console.log(4); }
+getName = function() { console.log(4); }
 function getName() { console.log(5); }
 
 Foo.getName();
 getName();
-Foo().getName();
+Foo().getName();     //window全局对象 会报错 浏览器可以运行
 getName();
 new Foo.getName();
 new Foo().getName();
