5362ff67e1f1339a59459663fcb3ed0512df07ad
jingjing20 <1269928993@qq.com>
2020-04-04 23:55:09 +0800

    this指向问题

---
+++ interview/this/bind.js
@@ -0,0 +1,34 @@
+Function.prototype.myBind = function (thisObj, ...arg1) {
+  // 第一次调用传参
+  let fn = this;
+  function jingbind(...arg2) {
+    const arg = arg1.concat(arg2);
+    let isjing = this instanceof jingbind;
+    return fn.apply(isjing ? this : thisObj, arg);
+  }
+  jingbind.prototype = fn.prototype;
+  return jingbind;
+}
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+const sum10 = sum.myBind(null, 10);
+let jing = sum10(20, 30);
+console.log(jing)
+
+function Animal(name, color) {
+  this.name = name;
+  this.color = color;
+}
+Animal.prototype.say = function () {
+  return `I'm a ${this.color} ${this.name}`;
+};
+const Cat = Animal.myBind(null, 'cat');
+const cat = new Cat('white');
+console.log(cat, cat.say())
+if (cat.say() === 'I\'m a white cat' &&
+  cat instanceof Cat && cat instanceof Animal) {
+  console.log('success');
+}
