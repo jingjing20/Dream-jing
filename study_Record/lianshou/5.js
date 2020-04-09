ed42761914f1b8f36670aa009e7a08ed5a4a79d7
jingjing20 <1269928993@qq.com>
2020-04-09 19:46:13 +0800

    react

--- study_Record/lianshou/5.js
+++ study_Record/lianshou/5.js
@@ -7,17 +7,24 @@
 // };
 // console.log(a == 101 && a == 102);//true
 
-var a = 1;
-function foo() {
-  var a = 2;
-  function baz() {
-    console.log(a);
-  }
-  bar(baz);
-}
-function bar(fn) {
-  // 这就是闭包
-  fn();
-}
-// 输出2，而不是1
-foo();
+// var a = 1;
+// function foo() {
+//   var a = 2;
+//   function baz() {
+//     console.log(a);
+//   }
+//   bar(baz);
+// }
+// function bar(fn) {
+//   // 这就是闭包
+//   fn();
+// }
+// // 输出2，而不是1
+// foo();
+// var a = { n: 1 };
+// var b = a;
+// a.x = a = { n: 2 };
+// console.log(a.x);
+// console.log(b.x);
+
+console.log(__dirname);
--- study_Record/lianshou/事件循环/1.js
+++ study_Record/lianshou/事件循环/1.js
@@ -1,46 +1,49 @@
-console.log('1');
+// console.log('1');
 
-setTimeout(function() {
-    console.log('2');
-    process.nextTick(function() {
-        console.log('3');
-    })
-    new Promise(function(resolve) {
-        console.log('4');
-        resolve();
-    }).then(function() {
-        console.log('5')
-    })
-})
-process.nextTick(function() {
-    console.log('6');
-})
-new Promise(function(resolve) {
-    console.log('7');
-    resolve();
-}).then(function() {
-    console.log('8')
-})
+// setTimeout(function() {
+//     console.log('2');
+//     process.nextTick(function() {
+//         console.log('3');
+//     })
+//     new Promise(function(resolve) {
+//         console.log('4');
+//         resolve();
+//     }).then(function() {
+//         console.log('5')
+//     })
+// })
+// process.nextTick(function() {
+//     console.log('6');
+// })
+// new Promise(function(resolve) {
+//     console.log('7');
+//     resolve();
+// }).then(function() {
+//     console.log('8')
+// })
 
-setTimeout(function() {
-    console.log('9');
-    process.nextTick(function() {
-        console.log('10');
-    })
-    new Promise(function(resolve) {
-        console.log('11');
-        resolve();
-    }).then(function() {
-        console.log('12')
-    })
-})
+// setTimeout(function() {
+//     console.log('9');
+//     process.nextTick(function() {
+//         console.log('10');
+//     })
+//     new Promise(function(resolve) {
+//         console.log('11');
+//         resolve();
+//     }).then(function() {
+//         console.log('12')
+//     })
+// })
 
-// 按照规定 能发起宏观任务的方法有：
+// // 按照规定 能发起宏观任务的方法有：
 
-// script(整体代码)、setTimeout、setInterval、I/O、UI交互事件、
-// postMessage、MessageChannel、setImmediate(Node.js 环境)
+// // script(整体代码)、setTimeout、setInterval、I/O、UI交互事件、
+// // postMessage、MessageChannel、setImmediate(Node.js 环境)
 
-// 微观任务的方法有：
+// // 微观任务的方法有：
 
-// Promise.then、MutaionObserver、process.nextTick(Node.js 环境)
-// async/await实际上是promise+generator的语法糖，也就是promise，也就是微观任务；
+// // Promise.then、MutaionObserver、process.nextTick(Node.js 环境)
+// // async/await实际上是promise+generator的语法糖，也就是promise，也就是微观任务；
+
+console.log(process.cwd());
+console.log(__dirname)
