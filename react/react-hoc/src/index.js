a4fde6353a262db10699fe364f0bfcc364ae3f5c
jingjing20 <1269928993@qq.com>
2020-04-10 11:11:46 +0800

    throttle 节流函数学习

--- react/react-hoc/src/index.js
+++ react/react-hoc/src/index.js
@@ -7,6 +7,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <button id="add">add</button>
+    <button id="min">min</button>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +17,39 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+window.onload = function () {
+  const add = document.getElementById('add');
+  const min = document.getElementById('min');
+  const delay = 2000;
+  // let prev = Date.now();
+  add.onclick = throttle(function () {
+    // let now = Date.now();
+    // if (now - prev > delay) {
+    console.log('jingjing')
+    //   prev = now;
+    // }
+  }, delay)
+
+  min.onclick = throttle(function () {
+    // let now = Date.now();
+    // if (now - prev > delay) {
+    console.log('haohao')
+    //   prev = now;
+    // }
+  }, delay)
+
+
+  //节流函数
+  function throttle(func, wait) {
+    let prev = Date.now();
+    // 里面这层函数才是返回给调用者执行的
+    return function () {
+      let now = Date.now();
+      if (now - prev > wait) {
+        func();
+        prev = now;
+      }
+    }
+  }
+}
