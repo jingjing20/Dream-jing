71ecd5cea51d0e2c0e1c89f046b1766f0d8ac488
jingjing20 <1269928993@qq.com>
2020-04-20 17:20:29 +0800

    react 学习

---
+++ react/lemon_hooks/src/index.tsx
@@ -0,0 +1,10 @@
+// import "./app.css";
+// console.log('welcome');
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import { App } from './app';
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+)
