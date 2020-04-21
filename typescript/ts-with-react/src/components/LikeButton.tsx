9dafe64fea0825ef1a4c2f5d6980051752f9be01
jingjing20 <1269928993@qq.com>
2020-04-21 22:40:55 +0800

    useRef

--- typescript/ts-with-react/src/components/LikeButton.tsx
+++ typescript/ts-with-react/src/components/LikeButton.tsx
@@ -1,19 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useMousePosition from '../hooks/useMousePosition';
 const LikeButton: React.FC = () => {
   const [like, setlike] = useState(0)
-  const [on, setOn] = useState(true)
+  const likeref = useRef(0)
   const positions = useMousePosition()
   useEffect(() => {
     document.title = `点击了${like}次`
   })
+  function handleAlertClick() {
+    setTimeout(() => {
+      alert('you clicked on ' + likeref.current)
+    }, 3000)
+  }
   return (
     <>
-      <button onClick={() => { setlike(like + 1) }}>
+      <button onClick={() => { setlike(like + 1); likeref.current++ }}>
         {like}
       </button>
-      <button onClick={() => { setOn(!on) }}>
-        {on ? 'on' : 'off'}
+      <button onClick={handleAlertClick}>
+        alert
       </button>
       <h1>X: {positions.x}, Y : {positions.y}</h1>
     </>
