b3bbb8215d5ba3d0d5e1a04c0c2509d02e5f6a1d
jingjing20 <1269928993@qq.com>
2020-05-21 17:55:43 +0800

    electron

---
+++ electron/electron-control/app/renderer/src/main/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Electron 调用 NativeAPI
+import { ipcRenderer } from 'electron'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>jing</h1>
+    </div>
+  );
+}
+
+export default App;
