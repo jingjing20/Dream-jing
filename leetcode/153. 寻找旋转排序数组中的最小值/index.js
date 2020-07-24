ccd858ef9201613873a63ccbd46149b17b435788
jingjing20 <1269928993@qq.com>
2020-07-24 10:11:40 +0800

    寻找旋转排序数组中的最小值

---
+++ leetcode/153. 寻找旋转排序数组中的最小值/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMin(nums) {
+  for(let i = 1; i < nums.length; i++) {
+    if(nums[i-1] > nums[i]) {
+      return nums[i]
+    }
+  }
+  return nums[0]
+}
+
+console.log(findMin([3,4,5,1,2]))
+console.log(findMin([0,1,2,3,4,5]))
