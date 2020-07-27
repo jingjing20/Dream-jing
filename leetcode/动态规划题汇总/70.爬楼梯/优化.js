c3dc069ebc97b65bfb261d6a96f7f74dcca0040c
jingjing20 <1269928993@qq.com>
2020-07-27 17:52:28 +0800

    爬楼梯优化

---
+++ leetcode/动态规划题汇总/70.爬楼梯/优化.js
@@ -0,0 +1,22 @@
+//这一版没有重复计算
+function climbStairs(n) {
+  if(n === 0) {
+    return 0;
+  }
+  if(n === 1) {
+    return 1;
+  }
+  if(n === 2) {
+    return 2;
+  }
+  let beforeValue = 1,afterValue = 2;
+  let result;
+  for(let i = 3; i <= n; i++) {
+    result = beforeValue + afterValue;
+    beforeValue = afterValue;
+    afterValue = result;
+  }
+  return result;
+}
+
+console.log(climbStairs(5))
