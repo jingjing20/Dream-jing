ba16818e0f1321ff3810f0ce7add27035774ecc0
jingjing20 <1269928993@qq.com>
2020-08-10 10:19:22 +0800

    update

--- leetcode/53. 最大子序和/review.js
+++ leetcode/53. 最大子序和/review.js
@@ -1,27 +1,15 @@
-// 思路
-
-// 动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
-// 如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
-// 如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
-// 每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
-// 时间复杂度：O(n)
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 
 function findMaxArray(nums) {
-  let sum = 0,ans = nums[0];
-  for(let num of nums) {
-    if(sum > 0) {
-      sum = sum + num
-    }else {
-      sum = num
-    }
-    ans = Math.max(sum,ans)
+  let ans = nums[0]
+  for(let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i] + Math.max(nums[i-1],0)
+    ans = Math.max(nums[i],ans)
   }
   return ans
 }
 
-console.log(findMaxArray([-1]))
+console.log(findMaxArray([-1,2,6,-3,5,-1]))
--- leetcode/剑指offer/42. 连续子数组的最大和/index.js
+++ leetcode/剑指offer/42. 连续子数组的最大和/index.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  
+  let ans = nums[0];
+  for(let i = 1; i <= nums.length - 1; i++) {
+      nums[i] += Math.max(nums[i-1],0);
+      ans = Math.max(ans,nums[i])
+  }
+  return ans;
 };
