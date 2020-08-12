e560a467485bac5fc268e3c1fe98151aea428afc
jingjing20 <1269928993@qq.com>
2020-08-12 20:03:29 +0800

    jing

--- leetcode/88.合并两个有序数组/1.js
+++ leetcode/88.合并两个有序数组/1.js
@@ -3,7 +3,7 @@
  * @param {number} m
  * @param {number[]} nums2
  * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @return {void} 
  */
 var merge = function (nums1, m, nums2, n) {
   let len1 = m - 1;
