7c856a7380bea56ff8964a017b29d8cf313029c5
jingjing20 <1269928993@qq.com>
2020-10-13 23:37:12 +0800

    code review

---
+++ leetcode/链表相关题/24. 两两交换链表中的节点/review.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function swapPairs(root) {
+  const dummyHead = new ListNode(0);
+  dummyHead.next = root;
+  let temp = dummyHead;
+  while(temp.next !== null && temp.next.next !== null) {
+    const node1 = temp.next;
+    const node2 = temp.next.next;
+    temp.next = node2;
+    node1.next = node2.next;
+    node2.next = node1;
+    temp = node1;
+  }
+  return dummyHead.next;
+}
