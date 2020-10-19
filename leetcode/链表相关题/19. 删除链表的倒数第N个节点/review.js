463d0dab25d89d1e444e9fb049c401051e47d9ab
jingjing20 <1269928993@qq.com>
2020-10-19 22:58:06 +0800

    review

---
+++ leetcode/链表相关题/19. 删除链表的倒数第N个节点/review.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  const dummyHead = new ListNode(0);  // 创建一个哑节点
+  dummyHead.next = head;
+  let left = dummyHead;
+  let right = dummyHead;
+  let time = n + 1
+  while(time--) {
+    right = right.next;
+  }
+
+  while(right) {
+    right = right.next;
+    left = left.next;
+  }
+
+  left.next = left.next.next;
+  return dummyHead.next;
+}
