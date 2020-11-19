5594be20b42a5e8bd87e5e673a2df4b9f482bbbf
jingjing20 <1269928993@qq.com>
2020-11-19 13:07:33 +0800

    leetcode 283

---
+++ leetcode/283. 移动零/index.js
@@ -0,0 +1,13 @@
+var moveZeroes = function (nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
+}
