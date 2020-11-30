1e1671f8d34e5b7b738082f68b8675163c447ebc
jingjing20 <1269928993@qq.com>
2020-11-30 15:32:43 +0800

    leetcode 454:四数相加2

---
+++ leetcode/454. 四数相加 II/review.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @param {number[]} C
+ * @param {number[]} D
+ * @return {number}
+ */
+var fourSumCount = function (A, B, C, D) {};
+let A = [1, 2];
+let B = [-2, -1];
+let C = [-1, 2];
+let D = [0, 2];
+console.log(fourSumCount(A, B, C, D));
