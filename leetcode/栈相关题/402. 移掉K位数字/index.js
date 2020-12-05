79bc5e2019800b86bd1baf3cae5a22e2e28f3141
jingjing20 <1269928993@qq.com>
2020-12-05 10:59:08 +0800

    review 移掉k个数字

--- leetcode/栈相关题/402. 移掉K位数字/index.js
+++ leetcode/栈相关题/402. 移掉K位数字/index.js
@@ -14,23 +14,24 @@
  * @return {string}
  */
 var removeKdigits = function (num, k) {
-  let stack = [];
-  for (let i = 0; i < num.length; i++) {
-    const c = num[i];
-    while (k > 0 && stack.length && stack[stack.length - 1] > c) {
-      stack.pop();
-      k--;
-    }
-    if (c != '0' || stack.length > 0) {
-      stack.push(c);
-    }
-  }
-  while (k > 0) {
-    stack.pop();
-    k--;
-  }
-  return stack.length === 0 ? '0' : stack.join('');
+	let stack = [];
+	for (let i = 0; i < num.length; i++) {
+		const c = num[i];
+		while (k > 0 && stack.length && stack[stack.length - 1] > c) {
+			stack.pop();
+			k--;
+		}
+		if (c != '0' || stack.length > 0) {
+			stack.push(c);
+		}
+	}
+	while (k > 0) {
+		stack.pop();
+		k--;
+	}
+	return stack.length === 0 ? '0' : stack.join('');
 };
 
-let num = "1432219", k = 3;
+let num = '1432219',
+	k = 3;
 console.log(removeKdigits(num, k));
--- leetcode/栈相关题/402. 移掉K位数字/review.js
+++ leetcode/栈相关题/402. 移掉K位数字/review.js
@@ -4,18 +4,31 @@
 // 输出: "1219"
 // 解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
 
-// 来源：力扣（LeetCode）
-// 链接：https://leetcode-cn.com/problems/remove-k-digits
-// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-
 /**
  * @param {string} num
  * @param {number} k
  * @return {string}
+ * 思路如下：
+ * 1、数组模拟栈，在遍历过程中维护一个单调栈，这里是想要留下的数字最小，所以当栈顶元素大于当前元素的时候把栈顶元素 pop() 掉，同时 k-1。
+ * 2、当遍历完成后 如果 k > 0，就继续从数组里面 pop() 元素，直到 k = 0。
  */
 var removeKdigits = function (num, k) {
-  
+	let stack = [];
+	for (let i = 0; i < num.length; i++) {
+		const c = num[i];
+		while (k > 0 && stack.length && stack[stack.length - 1] > c) {
+			stack.pop();
+			k--;
+		}
+		stack.push(c);
+	}
+	while (k > 0) {
+		stack.pop();
+		k--;
+	}
+	return stack.length === 0 ? '0' : stack.join('');
 };
 
-let num = "1432219", k = 3;
+let num = '1432219',
+	k = 3;
 console.log(removeKdigits(num, k));
