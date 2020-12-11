e446554e9235ef8345e3543cd8a7d40a16a037dd
jingjing20 <1269928993@qq.com>
2020-12-11 23:50:00 +0800

    review

--- leetcode/118. 杨辉三角/review.js
+++ leetcode/118. 杨辉三角/review.js
@@ -5,7 +5,7 @@
 var generate = function (numRows) {
 	let res = [];
 	for (let i = 0; i < numRows; i++) {
-		let row = new Array(i + 1).fill(1);
+		const row = new Array(i + 1).fill(1);
 		for (let j = 1; j < row.length - 1; j++) {
 			row[j] = res[i - 1][j] + res[i - 1][j - 1];
 		}
--- leetcode/84. 柱状图中最大的矩形/review.js
+++ leetcode/84. 柱状图中最大的矩形/review.js
@@ -5,12 +5,12 @@
 
 var largestRectangleArea = function (heights) {
 	let maxArea = 0;
-	let stack = []; // 数组模拟栈来记录方块的索引
+	let stack = [];
 	heights = [0, ...heights, 0];
 	for (let i = 0; i < heights.length; i++) {
 		while (heights[i] < heights[stack[stack.length - 1]]) {
 			const stackTopIndex = stack.pop();
-			maxArea = Math.max(maxArea, heights[stackTopIndex] * (i - stack[stack.length - 1] - 1));
+			maxArea = Math.max(maxArea, heights[stackTopIndex] * (i - stack[stack.length - 1] - 1)); // 因为前面从 stack 中 pop() 了要操作的柱子，所以stack的length减小了1，所以需要多减一个1才能准确的计算出底边的长（两个柱子索引之差）
 		}
 		stack.push(i);
 	}
