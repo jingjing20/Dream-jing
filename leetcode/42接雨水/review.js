2e11424d1b79d16a7bd436edcee432a7d63d697a
jingjing20 <1269928993@qq.com>
2020-12-14 11:05:24 +0800

    review

--- leetcode/42接雨水/review.js
+++ leetcode/42接雨水/review.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var trap = function (height) {
-	let res = 0;
-	let length = height.length;
-	for (let i = 1; i < length - 1; i++) {
+	let count = 0;
+	for (let i = 1; i < height.length - 1; i++) {
 		let max_left = 0,
 			max_right = 0;
+		for (let j = i; j < height.length; j++) {
+			max_right = Math.max(max_right, height[j]);
+		}
 		for (let j = i; j >= 0; j--) {
 			max_left = Math.max(max_left, height[j]);
 		}
-		for (let j = i; j < length; j++) {
-			max_right = Math.max(max_right, height[j]);
-		}
-		res += Math.min(max_left, max_right) - height[i];
+		count += Math.min(max_left, max_right) - height[i];
 	}
-	return res;
+	return count;
 };
 
 let height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
