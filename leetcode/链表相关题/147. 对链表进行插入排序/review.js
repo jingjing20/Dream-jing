997badefe514fc7e2343db44fce79a453882054c
jingjing20 <1269928993@qq.com>
2021-01-09 14:20:42 +0800

    review

--- leetcode/链表相关题/147. 对链表进行插入排序/review.js
+++ leetcode/链表相关题/147. 对链表进行插入排序/review.js
@@ -10,27 +10,28 @@
  * @return {ListNode}
  */
 var insertionSortList = function (head) {
-    if (head === null) return null;
-    let dummyHead = new ListNode(0);
-    dummyHead.next = head;
-    let temp = null;
-    let prev = null;
-    let cur = dummyHead.next;
-    while (cur && cur.next) {
-        if (cur.val <= cur.next.val) {
-            cur = cur.next;
-        } else {
-            temp = cur.next;        // 保存将要重新插入排序的节点
-            cur.next = cur.next.next;  // 删除当前节点
+	if (head === null) return null;
+	let dummyHead = new ListNode(0);
+	dummyHead.next = head;
+	let temp = null;
+	let prev = null;
+	let cur = dummyHead.next;
+	while (cur && cur.next) {
+		if (cur.val <= cur.next.val) {
+			cur = cur.next;
+		} else {
+			temp = cur.next; // 保存将要重新插入排序的节点
+			cur.next = cur.next.next; // 删除当前节点
 
-            prev = dummyHead;
-            while (prev.next.val <= temp.val) {
-                prev = prev.next;
-            }
+			prev = dummyHead;
+			while (prev.next.val <= temp.val) {
+				prev = prev.next;
+			}
 
-            temp.next = prev.next;
-            prev.next = temp;
-        }
-    }
-    return dummyHead.next;
+			// 将 temp 节点插入到 prev 和 prev.next 之间
+			temp.next = prev.next;
+			prev.next = temp;
+		}
+	}
+	return dummyHead.next;
 };
