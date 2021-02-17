9c3bfbeaacf3530bfecb8955de330d3a109d0329
jingjing20 <1269928993@qq.com>
2021-02-17 11:22:31 +0800

    update

--- interview/js/call-apply-bind/jing.js
+++ interview/js/call-apply-bind/jing.js
@@ -1,26 +1,25 @@
-Function.prototype.myBind = function(ObjThis, ...params) {
-  let fn = this;
-  function jingBind(...paramsTwo) {
-    const arg = params.concat(paramsTwo);
-    let isNew = this instanceof jingBind;
-    return fn.apply(isNew ? this : ObjThis, arg);
-  }
+Function.prototype.myBind = function (ObjThis, ...params) {
+	let fn = this;
+	function jingBind(...paramsTwo) {
+		const arg = params.concat(paramsTwo);
+		let isNew = this instanceof jingBind;
+		return fn.apply(isNew ? this : ObjThis, arg);
+	}
 
-  jingBind.prototype = Object.create(fn.prototype);
-  return jingBind;
-}
+	jingBind.prototype = Object.create(fn.prototype);
+	return jingBind;
+};
 
 function Animal(name, color) {
-  this.name = name;
-  this.color = color;
+	this.name = name;
+	this.color = color;
 }
 Animal.prototype.say = function () {
-  return `I'm a ${this.color} ${this.name}`;
+	return `I'm a ${this.color} ${this.name}`;
 };
 const Cat = Animal.myBind(null, 'cat');
 const cat = new Cat('white');
-console.log(cat, cat.say())
-if (cat.say() === 'I\'m a white cat' &&
-  cat instanceof Cat && cat instanceof Animal) {
-  console.log('success');
+console.log(cat, cat.say());
+if (cat.say() === "I'm a white cat" && cat instanceof Cat && cat instanceof Animal) {
+	console.log('success');
 }
--- interview/js/this/call.js
+++ interview/js/this/call.js
@@ -1,20 +1,20 @@
 Function.prototype.Call = function (thisObj, ...arg) {
-  if (!thisObj) {
-    //thisObj为null或者是undefined
-    thisObj = typeof window === 'undefined' ? global : window;
-  }
-  thisObj.fn = this;
-  // let rest = [...arguments].slice(1);//获取除了this指向对象以外的参数, 空数组slice后返回的仍然是空数组
-  return thisObj.fn(...arg); //隐式绑定,当前函数的this指向了context.
-}
+	if (!thisObj) {
+		//thisObj为null或者是undefined
+		thisObj = typeof window === 'undefined' ? global : window;
+	}
+	thisObj.fn = this;
+	// let rest = [...arguments].slice(1);//获取除了this指向对象以外的参数, 空数组slice后返回的仍然是空数组
+	return thisObj.fn(...arg); // 隐式绑定,当前函数的this指向了context.
+};
 var foo = {
-  name: 'Selina'
-}
+	name: 'Selina',
+};
 var name = 'Chirs';
-name = 'jingjing'
+name = 'jingjing';
 function bar(job, age) {
-  console.log(this.name);
-  console.log(job, age);
+	console.log(this.name);
+	console.log(job, age);
 }
 bar.Call(foo, 'programmer', 20);
 // Selina programmer 20
