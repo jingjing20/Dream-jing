24e5dd945850fbbef31c279dcbdba14673727965
jingjing20 <1269928993@qq.com>
2021-03-07 22:59:43 +0800

    update

--- leetcode/分治思想/53. 最大子序和/index.js
+++ leetcode/分治思想/53. 最大子序和/index.js
@@ -1,4 +1,4 @@
-// 只需要
+// 只需要保证在当前位置的最大值，然后遍历，前一位大于 0 就加上，小于 0 就加上 0。
 
 /**
  * @param {number[]} nums
@@ -8,7 +8,7 @@
 function findMaxArray(nums) {
 	let ans = nums[0];
 	for (let i = 1; i < nums.length; i++) {
-		nums[i] = nums[i] + Math.max(nums[i - 1], 0);
+		nums[i] = nums[i] + Math.max(nums[i - 1], 0); // 修改了每一位的最大值
 		ans = Math.max(nums[i], ans);
 	}
 	return ans;
