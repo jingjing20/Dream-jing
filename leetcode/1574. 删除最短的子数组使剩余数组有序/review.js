2cd8987435bcc0ccc72e66830427a11049642807
jingjing20 <1269928993@qq.com>
2021-03-21 20:12:08 +0800

    review LeetCode 1574

--- leetcode/1574. 删除最短的子数组使剩余数组有序/review.js
+++ leetcode/1574. 删除最短的子数组使剩余数组有序/review.js
@@ -5,20 +5,20 @@
 var findLengthOfShortestSubarray = function (arr) {
 	let n = arr.length;
 	let left = 0;
-	// 找到左边有序的最长序列
-	while (left + 1 < n && arr[left] <= arr[left + 1]) {
+	// 找到左边最长的有序序列
+	while (left + 1 <= n && arr[left] <= arr[left + 1]) {
 		left++;
 	}
-	if (left === n - 1) {
-		return 0;
-	}
-	// 找到右边有序的最长序列
+
+	if (left === n - 1) return 0;
+
+	// 找到右边最长的有序序列
 	let right = n - 1;
-	while (right > 0 && arr[right - 1] <= arr[right]) {
+	while (right >= 0 && arr[right - 1] <= arr[right]) {
 		right--;
 	}
-	// 最小值
-	let result = Math.min(n - left - 1, right);
+
+	let result = Math.min(right, n - left - 1);
 
 	// 考虑两边都取一部分元素
 	let i = 0,
