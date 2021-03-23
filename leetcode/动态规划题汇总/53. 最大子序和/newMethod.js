92d1906aedd3bb71d930f25ac603de650efb2b56
jingjing20 <1269928993@qq.com>
2021-03-24 00:18:15 +0800

    review

--- leetcode/动态规划题汇总/53. 最大子序和/newMethod.js
+++ leetcode/动态规划题汇总/53. 最大子序和/newMethod.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let ans = nums[0];
-  const n = nums.length;
-  for (let i = 1; i < n; i++) {
-    nums[i] += Math.max(nums[i-1],0);
-    ans = Math.max(ans, nums[i]);
-  }
-  return ans;
-}
+	let ans = nums[0];
+	for (let i = 1; i < nums.length; i++) {
+		nums[i] = nums[i] + Math.max(nums[i - 1], 0);
+		console.log(nums[i]); //jing-log
+		ans = Math.max(ans, nums[i]);
+	}
+	return ans;
+};
 
 const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-console.log(maxSubArray(arr));  // 6
+console.log(maxSubArray(arr)); // 6
